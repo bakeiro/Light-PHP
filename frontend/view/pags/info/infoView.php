fd984f7b159fa581e5b0f59d300b3e1318a4de7f
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2018-03-03 18:19:27 +0100

    Delete useless files

--- frontend/view/pags/info/infoView.php
+++
@@ -1,8 +0,0 @@
-<div class="content">
-
-    <div class="bd-callout bd-callout-warning">
-        <h4 id="conveying-meaning-to-assistive-technologies"><?=$title?></h4>
-        <p><?=$body?></p>
-    </div>
-
-</div>
--- frontend/view/pags/products/productsView.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-foreach($products as $product){ ?>
-
-    <div class="bd-callout bd-callout-warning">
-        <h4 id="conveying-meaning-to-assistive-technologies"><?=$product['name']?></h4>
-        <p><?=$product['detail_name']?></p>
-    </div>
-<?php } ?>
