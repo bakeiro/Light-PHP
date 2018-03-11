125c954a90921a29b82b5913bb119db66a525497
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2018-03-11 12:57:55 +0100

    not found page if incorrect rest url

--- frontend/controller/api/restController.php
+++ frontend/controller/api/restController.php
@@ -9,9 +9,11 @@ class restController{
 
 		$rest_controller = new Controller();
 		$output = $rest_controller->execRest();
-		
-        header('Content-Type: application/json');
-        echo json_encode($output);
+
+		if($rest_controller->class !== "errorController"){
+			header('Content-Type: application/json');
+			echo json_encode($output);
+		}
 	}
 
 }
