289c8fa7d93cad616f7a944d4d267fca75642e44
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2018-03-30 15:32:23 +0200

    Separate console

--- site/view/template/common/Footer.php
+++ site/view/template/common/Footer.php
@@ -1,49 +1,6 @@
 		</div>
 	</div>
 
-	<!-- Console -->
-	<?php
-		if(Settings::Get("debug")){
-
-		//Time, memory...
-		$memory = Util::convert(memory_get_usage(true));
-		
-		$end_time = microtime(true);
-		$time_script = $end_time - Settings::get("start_time");
-		$time_script = round($time_script, 4);
-	?>
-
-	<div id="error-console">
-
-		<div id="error-console-top"></div>
-		<button id="error-console-button" >Open</button>
-		
-		<span id="error-console-script-time"> <i class="material-icons">access_time</i>  <?=$time_script;?> </span>
-		<span id="error-console-script-memory"> <i class="material-icons">dns</i>  <?=$memory;?> </span>
-		
-		<div id="error-console-body">
-			<?php
-			foreach(Errors::$exceptions as $exception){
-				
-				if($exception["type"] === "error"){
-					echo "<p><i class='material-icons red-text'>error</i>".$exception["text"]."</p>";
-				}
-				if($exception["type"] === "warning"){
-					echo "<p><i class='material-icons lime-text'>warning</i>".$exception["text"]."</p>";
-				}
-			}
-			?>
-		</div>
-	</div>
-
-	<script src="site/view/www/<?=$cache?>/console/console.js"></script>
-	<link rel="stylesheet" href="site/view/www/<?=$cache?>/console/console.css">
-
-	<?php
-		}
-	?>
-
-
 	<?php
 
 	//Custom CSS/JS
@@ -53,6 +10,13 @@
 	foreach(Loader::$scripts as $script){
 		echo $script;
 	}
+	?>
+
+	<!-- Console -->
+	<?php
+		if(Settings::Get("debug")){
+			require(VIEW."template/common/Console.php");
+		}
 	?>
 
 	<script>
