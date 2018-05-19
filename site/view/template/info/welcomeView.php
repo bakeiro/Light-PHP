508b2d05f472d79169a3877f0841e17db16e65e0
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2018-05-19 14:12:19 +0200

    Update welcome page

--- site/view/template/info/welcomeView.php
+++ site/view/template/info/welcomeView.php
@@ -40,20 +40,22 @@ Created with a simple MVC structure, integrated with composer and npm, a console
 
 <h4>Features:</h4>
 <ul>
-	<li>Session managent</li>
-	<li>Npm integration</li>
-	<li>Composer integration</li>
-	<li>Gzip Compression (coming soon)</li>
-	<li>Seo Url (coming soon)</li>
-	<li>Self explained code</li>
-	<li>Easy structure</li>
-	<li>Social Login (coming soon)</li>
-	<li>Admin panel</li>
-	<li>Database included</li>
-	<li>Console to debug PHP errors</li>
-	<li>Php mailer integrated (send emails)</li>
-	<li>Error monitoring (coming soon)</li>
-	<li>Error logs</li>
-	<li>Easy rest</li>
-	<li>Cache control for CSS/JS files</li>	
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_0" checked="checked" /> <label for="checkbox_0" >Session managent</label> </li>
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_1" checked="checked" /> <label for="checkbox_1" >Npm integration</label> </li>
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_2" checked="checked" /> <label for="checkbox_2" >Composer integration</label> </li>
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_3" /> <label for="checkbox_3" >Gzip Compression (coming soon)</label> </li>
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_4" /> <label for="checkbox_4" >Seo Url (coming soon)</label> </li>
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_5" checked="checked" /> <label for="checkbox_5" >Self explained code</label></li>
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_6" checked="checked" /> <label for="checkbox_6" >Easy structure</label> </li>
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_7" /> <label for="checkbox_7" >Social Login (coming soon)</label> </li>
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_8" checked="checked" /> <label for="checkbox_8" >Admin panel</label></li>
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_9" checked="checked" /> <label for="checkbox_9" >Database included</label></li>
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_10" checked="checked" /> <label for="checkbox_10" >Console to debug PHP errors</label> </li>
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_11" checked="checked" /> <label for="checkbox_11" >Php mailer integrated (send emails)</label> </li>
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_12" /> <label for="checkbox_12" >Error monitoring (coming soon)</label> </li>
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_13" checked="checked" /> <label for="checkbox_13" >Error logs</label> </li>
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_14" checked="checked" /> <label for="checkbox_14" >Easy rest</label> </li>
+	<li> <input readonly type="checkbox" class="filled-in" id="checkbox_15" checked="checked" /> <label for="checkbox_15" >Cache control for CSS/JS files</label> </li>	
 </ul>
+
+<br><br>
