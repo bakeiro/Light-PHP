c87a46bf73f4bcc17f5c79baa99de7978a0f9f90
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2018-09-19 18:31:20 +0200

    Minor changes

--- site/view/template/common/Console.php
+++ site/view/template/common/Console.php
@@ -6,6 +6,9 @@
 		$end_time = microtime(true);
 		$time_script = $end_time - Settings::get("start_time");
 		$time_script = round($time_script, 4);
+
+		//Cache
+		$cache = Settings::Get("cache_version");
 	?>
 
 	<div id="error-console">
@@ -13,8 +16,8 @@
 		<div id="error-console-top"></div>
 		<button id="error-console-button" >Open</button>
 		
-		<span id="error-console-script-time"> <i class="material-icons">access_time</i>  {%time_script%} </span>
-		<span id="error-console-script-memory"> <i class="material-icons">dns</i>  {%memory%} </span>
+		<span id="error-console-script-time"> <i class="material-icons">access_time</i>  <?=$time_script?> </span>
+		<span id="error-console-script-memory"> <i class="material-icons">dns</i>  <?=$memory?> </span>
 		
 		<div id="error-console-body">
 			<?php
