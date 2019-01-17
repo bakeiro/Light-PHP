e071091cfce346a2f21edf7a795d40eb9450f352
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2019-01-17 19:09:09 +0100

    minor changes

--- database.sql
+++ database.sql
@@ -1,34 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 16-01-2019 a las 11:31:55
--- Versión del servidor: 10.1.25-MariaDB
--- Versión de PHP: 7.1.7
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `framework`
---
 CREATE DATABASE IF NOT EXISTS `framework` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 USE `framework`;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `product`
---
 
 CREATE TABLE `product` (
   `id` int(11) NOT NULL,
@@ -37,10 +13,6 @@ CREATE TABLE `product` (
   `image` varchar(255) COLLATE utf8_bin NOT NULL,
   `enable` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Volcado de datos para la tabla `product`
---
 
 INSERT INTO `product` (`id`, `quantity`, `model`, `image`, `enable`) VALUES
 (0, 20, 'V20-061-2a', 'tiles/A0001b.jpg', 1),
@@ -54,22 +26,12 @@ INSERT INTO `product` (`id`, `quantity`, `model`, `image`, `enable`) VALUES
 (8, 26, 'S019a', 'tiles/A0009b.jpg', 1),
 (9, 52, 'S018a', 'tiles/A0010b.jpg', 1);
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `product_info`
---
-
 CREATE TABLE `product_info` (
   `id` int(11) NOT NULL,
   `title` varchar(65) COLLATE utf8_bin DEFAULT NULL,
   `short_description` varchar(200) COLLATE utf8_bin DEFAULT NULL,
   `description` text COLLATE utf8_bin
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Volcado de datos para la tabla `product_info`
---
 
 INSERT INTO `product_info` (`id`, `title`, `short_description`, `description`) VALUES
 (0, 'Custom tile', 'cool tile', '&lt;p&gt;&lt;span style=&quot;color: rgb(0, 0, 0); font-family: Arial, Helvetica, sans-serif; font-size: 14px;&quot;&gt;Really The tile comes from Spain and bla bla bla.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;'),
@@ -83,12 +45,6 @@ INSERT INTO `product_info` (`id`, `title`, `short_description`, `description`) V
 (8, 'Custom tile', 'cool tile', '&lt;p&gt;&lt;span style=&quot;color: rgb(0, 0, 0); font-family: Arial, Helvetica, sans-serif; font-size: 14px;&quot;&gt;Nice product.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;'),
 (9, 'Custom tile', 'cool tile', '&lt;p&gt;&lt;span style=&quot;color: rgb(0, 0, 0); font-family: Arial, Helvetica, sans-serif; font-size: 14px;&quot;&gt;Really Nice product, the tile comes from Spain and bla bla bla.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;');
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `user`
---
-
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `first_name` varchar(80) COLLATE utf8_bin NOT NULL,
@@ -100,48 +56,20 @@ CREATE TABLE `user` (
   `address` varchar(45) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Volcado de datos para la tabla `user`
---
-
 INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `password`, `salt`, `role`, `address`) VALUES
 (1, 'Jonh', 'Doe', 'admin@email.com', 'd6674395604f7ae0a3750cf4a7280d623daf67d6', 'ySUrWaBHx', 'admin_master', ''),
 (2, 'David', 'Bak', 'customer@email.com', 'bdfe37077d9d67d6bec1d2dd8892c286fae37387', 'm7dcl8Acy', 'customer', '');
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `product`
---
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `model_UNIQUE` (`model`);
 
---
--- Indices de la tabla `product_info`
---
 ALTER TABLE `product_info`
   ADD PRIMARY KEY (`id`);
 
---
--- Indices de la tabla `user`
---
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email_UNIQUE` (`email`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `product`
---
 ALTER TABLE `product`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9399;COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
