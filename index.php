f5744c459560b5df162f5df363ac5ef0ef62c9ff
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2019-01-28 14:11:09 +0100

    Update the install file

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
 //Routes
-define('DIR_ROOT', 'C:/xampp/htdocs/framework_php/');
-define('SYSTEM', DIR_ROOT.'system/');
-define('MODEL', DIR_ROOT.'site/model/');
-define('CONTROLLER', DIR_ROOT.'site/controller/');
-define('VIEW', DIR_ROOT.'site/view/');
+define("DIR_ROOT", "C:/yourPath/yourApp/");
+define("SYSTEM", DIR_ROOT."system/");
+define("MODEL", DIR_ROOT."site/model/");
+define("CONTROLLER", DIR_ROOT."site/controller/");
+define("VIEW", DIR_ROOT."site/view/");
 
 //Config
 require(SYSTEM . "engine/Config.php");
@@ -13,16 +13,16 @@ require(SYSTEM . "config/config_data.php");
 require(SYSTEM . "config/php_settings.php");
 
 //Engine
-require(SYSTEM . 'engine/Url.php');
-require(SYSTEM . 'engine/Controller.php');
-require(SYSTEM . 'engine/Session.php');
-require(SYSTEM . 'engine/SessionSecureHandler.php');
+require(SYSTEM . "engine/Url.php");
+require(SYSTEM . "engine/Controller.php");
+require(SYSTEM . "engine/Session.php");
+require(SYSTEM . "engine/SessionSecureHandler.php");
 require(SYSTEM . "engine/Output.php");
-require(SYSTEM . 'engine/Database.php');
-require(SYSTEM . 'engine/Util.php');
-require(SYSTEM . 'engine/Errors.php');
-require(SYSTEM . 'engine/SecModel.php');
-require(SYSTEM . 'engine/SecController.php');
+require(SYSTEM . "engine/Database.php");
+require(SYSTEM . "engine/Util.php");
+require(SYSTEM . "engine/Errors.php");
+require(SYSTEM . "engine/SecModel.php");
+require(SYSTEM . "engine/SecController.php");
 
 //Startup
 require(SYSTEM. "startup.php");
@@ -33,5 +33,5 @@ $Controller->execController();
 
 //Finish script functions
 //Session::$handler->close();
-//Database::$CONN->close()
-//$pdo->query('SELECT pg_terminate_backend(pg_backend_pid());');
+//Database::$CONN->close();
+//Database::query("SELECT pg_terminate_backend(pg_backend_pid())");
