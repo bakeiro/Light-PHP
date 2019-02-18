630e4dcdd7d0a994ea9cd05f6b9ad25a308d0aae
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2019-02-18 15:58:00 +0100

    Updated the default settings

--- system/config/php_settings.example.php
+++ system/config/php_settings.example.php
@@ -5,27 +5,30 @@
  */
 
 //Errors
-ini_set('display_errors', 'On');
+ini_set("display_errors", "On");
 ini_set("log_errors", 1);
 
 //Session
-ini_set('session.cookie_lifetime', 0);
-ini_set('session.gc_maxlifetime', 14400); //4h
-ini_set("expose_php", "Off");
+ini_set("session.cookie_lifetime", 0);
+ini_set("session.gc_maxlifetime", 14400); //4h
+ini_set("session.auto_start", "Off");
 
+//Others
+ini_set("expose_php", "Off");
+ini_set("default_charset", "UTF-8");
+
+//Limits
+ini_set("max_input_time", 100);
+init_set("memory_limit", "64M");
+init_set("max_execution_time", 36000);
+init_set("upload_max_filesize", "999M");
+init_set("mysql.connect_timeout", 20);
 
 /*
 Common settings
-max_input_time = 200
 magic_quotes_gpc = Off
 register_globals = Off
-default_charset = UTF-8
-memory_limit = 64M
-max_execution_time = 36000
-upload_max_filesize = 999M
 safe_mode = Off
-mysql.connect_timeout = 20
-session.auto_start = Off
 session.cookie_lifetime = 3600
 allow_url_fopen = On
 error_reporting = E_ALL
