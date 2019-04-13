9bd5edc4fdc1537b78fe7edb9b11ae62dabfaf4f
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2019-04-13 16:39:27 +0200

    Delete redundant session files using the garbace recollector

--- system/engine/SessionSecureHandler.php
+++ system/engine/SessionSecureHandler.php
@@ -22,5 +22,32 @@ class SessionSecureHandler extends SessionHandler {
 		$encrypted_data = openssl_encrypt($data, $this->encrypt_method, $this->key, 0, $this->iv);
 		return parent::write($session_id, $encrypted_data);
 	}
+
+	//Methods for deleting remaining session files
+	public function destroy($session_id) {
+		$file = SYSTEM.'/writable/sessions/sess_'.basename($session_id);
+		if (is_file($file)) {
+			unlink($file);
+		}
+	}
+
+	//Methods for deleting remaining session files
+	public function __destruct() {
+		
+		$gc_divisor = ini_get('session.gc_divisor');
+		$gc_probability = ini_get('session.gc_probability');
+
+		if ((rand() % $gc_divisor) < $gc_probability) {
+			$expire = time() - ini_get('session.gc_maxlifetime');
+
+			$files = glob(SYSTEM . '/writable/sessions/sess_*');
+
+			foreach ($files as $file) {
+				if (filemtime($file) < $expire) {
+					unlink($file);
+				}
+			}
+		}
+	}
   
 }
