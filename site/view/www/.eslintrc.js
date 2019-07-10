f33179b8bd72e740add0c21cda22d4eb329896be
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2019-07-10 19:07:49 +0200

    added airbnb guidelines

---
+++ site/view/www/.eslintrc.js
@@ -0,0 +1,40 @@
+// eslint:recommended,
+// airbnb-base
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "airbnb-base",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly",
+        "$": true,
+        "window": true,
+        "console": true,
+        "e": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
