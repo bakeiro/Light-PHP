8a8d405eb6cecd644f22e8e0f794f19e5130c67c
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2019-08-07 22:58:15 +0200

    update console

--- site/view/www/src/console/console.js
+++ site/view/www/src/console/console.js
@@ -3,11 +3,11 @@ class ErrorConsole {
     this.errors = false;
     this.event_track = false;
     this.dragged = null;
-    this.opened = false;
     this.div_error_console = window.document.querySelector("div#error-console");
     this.error_console_top = window.document.querySelector("div#error-console-top");
     this.error_console_buttons = window.document.querySelectorAll("button#error-console-button");
     this.error_console_body = window.document.querySelector("div#error-console-body-debug");
+    //this.error_console_body_2 = window.document.querySelector("div#error-console-body");
   }
 
   activateError() {
@@ -22,16 +22,13 @@ class ErrorConsole {
 
   open(height = "200px") {
     this.div_error_console.style.height = height;
-    window.document.querySelector("div#error-console-body").style.height = height;
     sessionStorage.setItem("console_position", height);
   }
 
   close() {
-    this.div_error_console.style.height = "30px";
-    window.document.querySelector("div#error-console-body").style.height = "30px";
+    this.div_error_console.style.height = "35px";
     sessionStorage.setItem("console_position", "closed");
   }
-
 
   checkErrors() {
     const childs = this.error_console_body.children;
@@ -47,39 +44,31 @@ class ErrorConsole {
     }
 
     if (errors_found) {
-      $("button#error-console-button").css("background-color", "#F44336");
+      this.error_console_buttons.forEach((elem) => {
+        elem.style.backgroundColor = "#F44336";
+      });
     }
   }
 }
 
-const error_console = new ErrorConsole();
+window.debug_console = new ErrorConsole();
 
-$("body").on("click", "button#error-console-button", (e) => {
-  if (this.opened) {
-    error_console.open();
+$("body").on("click", "button#error-console-button", () => {
+  debugger;
+  if (sessionStorage.getItem("console_position") !== "closed") {
+    window.debug_console.close();
   } else {
-    error_console.close();
+    window.debug_console.open();
   }
 });
-
-// Move
-$("body").on("click", "div#error-console-top", (e) => {
-  const text = $("button#error-console-button").text();
-
-  if (text.search("Close") !== -1) {
-    $("div#error-console-top").css("background-color", "#ee6e73");
-    error_console.event_track = true;
-  }
-});
-
 
 // Check errors
 $(document).ready(() => {
-  error_console.checkErrors();
+  window.debug_console.checkErrors();
   // Height
   if (sessionStorage.getItem("console_position")) {
     if (sessionStorage.getItem("console_position") !== "closed") {
-      error_console.open(sessionStorage.getItem("console_position"));
+      window.debug_console.open(sessionStorage.getItem("console_position"));
     }
   }
 });
