35ae8026f881d1f02589d513026dabe218ee701f
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2019-08-07 21:05:01 +0200

    console update

--- site/view/www/src/console/console.js
+++ site/view/www/src/console/console.js
@@ -1,88 +1,63 @@
-const error_console = (() => {
-  const errors = false;
-  const eventTrack = false;
-  const dragged = null;
-
-  function activateError() {
-    $("button#error-console-button").css("background-color", "#F44336");
-    $("div#error-console-top").css("background-color", "#9E9E9E");
+class ErrorConsole {
+  constructor() {
+    this.errors = false;
+    this.event_track = false;
+    this.dragged = null;
+    this.opened = false;
+    this.div_error_console = window.document.querySelector("div#error-console");
+    this.error_console_top = window.document.querySelector("div#error-console-top");
+    this.error_console_buttons = window.document.querySelectorAll("button#error-console-button");
+    this.error_console_body = window.document.querySelector("div#error-console-body-debug");
   }
 
-  function open(height = "200px") {
-    $("div#error-console").css("height", height);
-    $("div#error-console-body").css("height", height);
-    $("button#error-console-button").text("Close");
+  activateError() {
+    this.error_console_buttons.forEach((elem) => {
+      elem.style.backgroundColor = "#F44336";
+      elem.style.border = "2px solid red";
+    });
 
+    const div_console = this.error_console_top[0];
+    div_console.style.backgroundColor = "#9E9E9E";
+  }
+
+  open(height = "200px") {
+    this.div_error_console.style.height = height;
+    window.document.querySelector("div#error-console-body").style.height = height;
     sessionStorage.setItem("console_position", height);
   }
 
-  function close() {
-    $("div#error-console").css("height", "30px");
-    $("div#error-console-body").css("height", "30px");
-    $("button#error-console-button").text("Open");
-
+  close() {
+    this.div_error_console.style.height = "30px";
+    window.document.querySelector("div#error-console-body").style.height = "30px";
     sessionStorage.setItem("console_position", "closed");
   }
 
-  function updateHeight() {
-    if (this.eventTrack) {
-      this.eventTrack = false;
-      $("div#error-console-top").css("background-color", "lightgrey");
-      $("div#error-console").css("height", this.dragged);
-      $("div#error-console-body").css("height", this.dragged);
-      sessionStorage.setItem("console_position", this.dragged);
-    }
-  }
 
-  function trackMouse() {
-    if (this.eventTrack) {
-      const posY = event.clientY;
-      const px_height = (620 - posY) + "px";
-      this.dragged = px_height;
-    }
-  }
-
-  function checkErrors() {
-    const childs = $("div#error-console-body").children();
-    let errorsFound = false;
+  checkErrors() {
+    const childs = this.error_console_body.children;
+    let errors_found = false;
 
     let i = 0;
     while (childs[i]) {
       if (childs[i].className === "warning" || childs[i].className === "error") {
-        errorsFound = true;
+        errors_found = true;
         break;
       }
       i += 1;
     }
 
-    if (errorsFound) {
+    if (errors_found) {
       $("button#error-console-button").css("background-color", "#F44336");
     }
   }
+}
 
-  return {
-    errors,
-    eventTrack,
-    dragged,
-    activateError,
-    open,
-    close,
-    trackMouse,
-    updateHeight,
-    checkErrors,
-  };
+const error_console = new ErrorConsole();
 
-})();
-
-
-// Button
 $("body").on("click", "button#error-console-button", (e) => {
-  const text = $("button#error-console-button").text();
-
-  if (text.search("Open") !== -1) {
+  if (this.opened) {
     error_console.open();
-  }
-  if (text.search("Close") !== -1) {
+  } else {
     error_console.close();
   }
 });
@@ -93,22 +68,15 @@ $("body").on("click", "div#error-console-top", (e) => {
 
   if (text.search("Close") !== -1) {
     $("div#error-console-top").css("background-color", "#ee6e73");
-    error_console.eventTrack = true;
+    error_console.event_track = true;
   }
 });
 
-$("body").on("mouseup", () => {
-  error_console.updateHeight();
-});
-
-$("body").on("mousemove", () => {
-  error_console.trackMouse();
-});
 
 // Check errors
 $(document).ready(() => {
   error_console.checkErrors();
-
+  // Height
   if (sessionStorage.getItem("console_position")) {
     if (sessionStorage.getItem("console_position") !== "closed") {
       error_console.open(sessionStorage.getItem("console_position"));
