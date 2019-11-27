81d9343663108161f2cc34211bd36d542f7d1746
David Baqueiro Santerbás <davidbaqueiro@outlook.com>
2019-11-27 13:28:26 +0100

    Update post-install.php

--- post-install.php
+++ post-install.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once("./system/engine/Util.php");
-$config_string = file_get_contents('config.readme.php');
+$config_string = file_get_contents('./config.rename.php');
 
 $token_iv = Util::generateSimpleToken(16);
 $config_string = str_replace("ThisIsMySecretIv", $token_iv , $config_string);
@@ -9,8 +9,8 @@ $config_string = str_replace("ThisIsMySecretIv", $token_iv , $config_string);
 $token_pass = Util::generateSimpleToken(32);
 $config_string = str_replace("ThisIsMySecretPass", $token_pass , $config_string);
 
-file_put_contents('config.readme.php', $config_string);
-rename('config.rename.php', 'config.php');
+file_put_contents('./config.rename.php', $config_string);
+rename('./config.rename.php', 'config.php');
 
 rename('php.rename.ini', 'php.ini');
 
