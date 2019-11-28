282df17069224c35e2c2fe855aeac286dfb9e27d
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2019-11-28 22:12:29 +0100

    reduce code

--- system/routes.php
+++ system/routes.php
@@ -1,11 +1,10 @@
 <?php
 
 /**
- * Here you can define the routes, the key it's the seo-friendly url, and the value it's the folder/class/method string which
- * specifies the method to be executed
+ * Array key => url
+ * Array value => folder/class/method (index method if none specified)
  */
 
 $routes = array();
 $routes["/"] = "index/index";
 $routes["/welcome"] = "index/index";
-$routes["/user"] = "account/customer/loginPage";
--- system/startup.php
+++ system/startup.php
@@ -16,9 +16,8 @@ if(Config::get("initialize_database")){
 
     try {
         $temp_con = new PDO("mysql:host=" .Config::get("CONN_HOST"). ";port=3306;dbname=" . Config::get("CONN_DDBB"), Config::get("CONN_USER"), Config::get("CONN_PASS"));
-        $temp_con->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //true prepare statements
+        $temp_con->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); // true prepare statements
 
-        //array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone ='".Config::get("default_time_zone")."'") //TODO: set timezone also for the database
         $temp_con->exec("SET NAMES 'utf8'");
         $temp_con->exec("SET CHARACTER SET utf8");
         $temp_con->exec("SET CHARACTER_SET_CONNECTION=utf8");
@@ -65,22 +64,12 @@ if(Config::get("show_debug_info")){
 	Config::set("whoops", false);
 }
 
-//Whoops
-if(Config::get("whoops")){
-	$whoops = new \Whoops\Run;
-	$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-	$whoops->register();
-}
-
 //Console info
 Config::set("console_db_queries", array());
 Config::set("console_warnings", array());
 Config::set("console_errors", array());
 Config::set("console_debug_info", array());
 Config::set("console_execution_trace", array());
-
-//Engine finished
-Config::set("loaded", true);
 
 //Track execution time
 Config::set("controller_execution_time", microtime(true));
