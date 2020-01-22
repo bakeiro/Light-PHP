768690d7044c0f027600127e440926b6bb81c317
David Baqueiro <dbaqueiro@CDDL.local>
2020-01-22 10:42:32 +0100

    minor change

--- post-install.php
+++ post-install.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once "./system/library/Util.php";
-$config_string = file_get_contents('./config.rename.php');
+$config_string = file_get_contents('/system/config/config.rename.php');
 
 $token_iv = Library\Util::generateSimpleToken(16);
 $config_string = str_replace("ThisIsMySecretIv", $token_iv, $config_string);
@@ -9,9 +9,9 @@ $config_string = str_replace("ThisIsMySecretIv", $token_iv, $config_string);
 $token_pass = Library\Util::generateSimpleToken(32);
 $config_string = str_replace("ThisIsMySecretPass", $token_pass, $config_string);
 
-file_put_contents('./config.rename.php', $config_string);
+file_put_contents('/system/config/config.rename.php', $config_string);
 
-rename('./config.rename.php', 'config.php');
+rename('/system/config/config.rename.php', '/system/config/config.php');
 rename('php.rename.ini', 'php.ini');
 rename('system/rename.writable', 'system/writable');
 
