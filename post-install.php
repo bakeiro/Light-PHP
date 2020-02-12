b7edf492253b7b403e9f5f3109b2f671dac28b35
David Baqueiro <dbaqueiro@CDDL.local>
2020-02-12 09:29:57 +0100

    tset

--- post-install.php
+++ post-install.php
@@ -11,7 +11,7 @@ $config_string = str_replace("ThisIsMySecretPass", $token_pass, $config_string);
 
 file_put_contents('./system/config/config.rename.php', $config_string);
 
-rename('./system/config/config.rename.php', './system/config/config.php');
+rename('./system/config/config_rename.php', './system/config/config.php');
 rename('php.rename.ini', 'php.ini');
 rename('system/rename_writable', 'system/writable');
 
