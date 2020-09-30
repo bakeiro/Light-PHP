7042afd9fe740b3fb44e3bd5a64d7c95ff2b7098
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2020-09-30 22:23:09 +0200

    Updates

--- system/tests/environmentTest.php
+++ system/tests/environmentTest.php
@@ -1,26 +1,41 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
-
 /**
  * Class for checking out if the environment pass all the test meaning it contains all the needed dependencies to run
  * Light-PHP
  */
 class EnvironmentTest extends TestCase
 {
-    public function testCheckLoadedExtensions(): void
+    public function testCheckLoadedExtensions(): boolean
     {
         $loaded_modules = get_loaded_extensions();
+        $loaded_all_dependencies = true;
 
-        $this->assertTrue(in_array("openssl", $loaded_modules));
-        $this->assertTrue(in_array("session", $loaded_modules));
-        $this->assertTrue(in_array("date", $loaded_modules));
-        $this->assertTrue(in_array("json", $loaded_modules));
-        $this->assertTrue(in_array("PDO", $loaded_modules));
+        if (!in_array("openssl", $loaded_modules)) {
+            $loaded_all_dependencies = false;
+        }
+
+        if (!in_array("session", $loaded_modules)) {
+            $loaded_all_dependencies = false;
+        }
+
+        if (!in_array("date", $loaded_modules)) {
+            $loaded_all_dependencies = false;
+        }
+
+        if (!in_array("json", $loaded_modules)) {
+            $loaded_all_dependencies = false;
+        }
+
+        if (!in_array("PDO", $loaded_modules)) {
+            $loaded_all_dependencies = false;
+        }
+
+        return $loaded_all_dependencies;
     }
 
-    public function testPHPVersion(): void
+    public function testPHPVersion(): bool
     {
-        $this->assertTrue(version_compare(phpversion(), '7.0', '>'));
+        return version_compare(phpversion(), '7.0', '>') ? true : false;
     }
 }
