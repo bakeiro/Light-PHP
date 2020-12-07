780a8c561044333f3688e917401ea2b0e2e5e2e9
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2020-12-07 18:16:33 +0100

    update

--- post-install.php
+++ post-install.php
@@ -23,7 +23,7 @@ if (!in_array("PDO", $loaded_modules)) {
     $loaded_all_dependencies = false;
 }
 
-$enough_php_version = version_compare(phpversion(), '7.0', '>') ? true : false;
+$enough_php_version = version_compare(phpversion(), '7.1', '>') ? true : false;
 
 require_once "./system/library/Util.php";
 $config_string = file_get_contents("./system/config/_config.php");
@@ -35,14 +35,10 @@ $config_string = str_replace("ThisIsMySecretIv", $token_iv, $config_string);
 $token_pass = $util->generateSimpleToken(32);
 $config_string = str_replace("ThisIsMySecretPass", $token_pass, $config_string);
 
-file_put_contents("./system/config/_config.php", $config_string);
+file_put_contents("./system/config/config.php", $config_string);
 
-rename("./system/config/_config.php", "./system/config/config.php");
-rename("./system/config/_ini.php", "./system/config/ini.php");
-
-mkdir("./system/writable");
-mkdir("./system/writable/logs");
-mkdir("./system/writable/upload");
+rename("./system/config/config.php", "./system/config/config.php");
+rename("./system/config/ini.php", "./system/config/ini.php");
 
 unlink("README.md");
 unlink("LICENSE.md");
